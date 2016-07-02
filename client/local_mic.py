bb59b64db8e35922daf40aeef0aba239db2fd5d2
Sujal <070bex442@ioe.edu.np>
2016-07-02 12:39:44 +0545

    updated

--- client/local_mic.py
+++ client/local_mic.py
@@ -4,13 +4,6 @@ A drop-in replacement for the Mic class that allows for all I/O to occur
 over the terminal. Useful for debugging. Unlike with the typical Mic
 implementation, Jasper is always active listening with local_mic.
 """
-import logging
-import tempfile
-import wave
-import audioop
-import pyaudio
-import alteration
-import jasperpath
 
 
 class Mic:
@@ -35,8 +28,5 @@ class Mic:
         self.prev = input
         return input
 
-       def say(self, phrase,
-            OPTIONS=" -vdefault+m3 -p 40 -s 160 --stdout > say.wav"):
-        # alter phrase before speaking
-        phrase = alteration.clean(phrase)
-        self.speaker.say(phrase)
+    def say(self, phrase, OPTIONS=None):
+        print("JASPER: %s" % phrase)
