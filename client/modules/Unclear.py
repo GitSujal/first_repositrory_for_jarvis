3aa11e37a699481526d96c350f3acb9076e48ec4
Sujal <070bex442@ioe.edu.np>
2016-08-15 12:05:15 +0545

    file write first try

--- client/modules/Unclear.py
+++ client/modules/Unclear.py
@@ -1,28 +1,30 @@
 # -*- coding: utf-8-*-
+import datetime
 from sys import maxint
 import random
+fileDir = os.path.dirname(os.path.realpath('__file__'))
+filename = "Missed_Commands.txt"
 
 WORDS = []
-
 PRIORITY = -(maxint + 1)
 
 
 def handle(text, mic, profile):
-    """
-        Reports that the user has unclear or unusable input.
-
-        Arguments:
-        text -- user-input, typically transcribed speech
-        mic -- used to interact with the user (for both input and output)
-        profile -- contains information related to the user (e.g., phone
-                   number)
-    """
-
+    date_string = datetime.datetime.now()
+    issue_time = str(date_string.year) +'-' + str(date_string.month) +'-' + str(date_string.day) +','+ str(date_string.hour) +':'+ str(date_string.minute) +':'+ str(date_string.second)
     messages = ["I'm sorry, could you repeat that?",
                 "My apologies, could you try saying that again?",
                 "Say that again?", "I beg your pardon?"]
 
     message = random.choice(messages)
+    filename = os.path.join(fileDir, '../../Logs/'+filename)
+    filename = os.path.abspath(os.path.realpath(filename))
+    with open(filename, "a") as myfile:
+    print("Name of the file: ", myfile.name)
+    print("Opening mode : ", myfile.mode)
+    myfile.write(text + '\t' +  issue_time )
+    myfile.close()
+    print("File Closed : ", myfile.closed)
 
     mic.say(message)
 
