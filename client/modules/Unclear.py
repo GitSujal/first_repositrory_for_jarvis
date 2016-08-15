2985a7454bca1791636127f153dadf6c39e1b8bc
Sujal <070bex442@ioe.edu.np>
2016-08-15 12:10:08 +0545

    Changed unclear.py

--- client/modules/Unclear.py
+++ client/modules/Unclear.py
@@ -2,6 +2,8 @@
 import datetime
 from sys import maxint
 import random
+import os
+
 fileDir = os.path.dirname(os.path.realpath('__file__'))
 filename = "Missed_Commands.txt"
 
@@ -14,7 +16,7 @@ def handle(text, mic, profile):
     issue_time = str(date_string.year) +'-' + str(date_string.month) +'-' + str(date_string.day) +','+ str(date_string.hour) +':'+ str(date_string.minute) +':'+ str(date_string.second)
     messages = ["I'm sorry, could you repeat that?",
                 "My apologies, could you try saying that again?",
-                "Say that again?", "I beg your pardon?"]
+                "Say that again?", "I beg your pardon?","Sorry I miss this command I would upload this request and hope next time you'll get execution for this command","Sorry Try any other command"]
 
     message = random.choice(messages)
     filename = os.path.join(fileDir, '../../Logs/'+filename)
