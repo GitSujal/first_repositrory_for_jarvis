99cfef46fbabc944e40e95f3281789b83d9603ba
Sujal <070bex442@ioe.edu.np>
2016-08-15 08:46:56 +0545

    music

--- client/modules/music.py
+++ client/modules/music.py
@@ -3,18 +3,9 @@ import re
 import random
 
 WORDS = ["PLAY","play","MUSIC","music","SONG","song"]
-PRIORITY = 2 
+PRIORITY = 1
 
 def handle(text, mic, profile):
-    """
-        Reports that the user has unclear or unusable input.
-
-        Arguments:
-        text -- user-input, typically transcribed speech
-        mic -- used to interact with the user (for both input and output)
-        profile -- contains information related to the user (e.g., phone
-                   number)
-    """
 
     messages = ["Sure sir Here is a music","Sure sir tell me which song","Ok"]
 
@@ -25,4 +16,4 @@ def handle(text, mic, profile):
 
 def isValid(text):
 	#TO know the input is for Selfie-Bot
-    return bool(re.search(r'\b(play|music|song|)\b', text, re.IGNORECASE))
+    return bool(re.search(r'\b(play|music|song|tone)\b', text, re.IGNORECASE))
