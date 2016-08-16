ac33e307cc6aece8bdffaf434e4c1fcbd6f1f3bd
Sujal <070bex442@ioe.edu.np>
2016-08-16 18:11:57 +0545

    Updated upload.py and unclear.py

--- upload.py
+++ upload.py
@@ -1,9 +1,10 @@
+
 import ftplib
 filename = "data.txt"
 ftp = ftplib.FTP('ftp.offerharu.com')
 ftp.login('jarvisai','minor#468&Net')
 ftp.cwd('Data')
 myfile = open(filename,'rb')
-ftp.storlines('STOR ' + filename , myfile)
+ftp.storlines('APPE ' + filename , myfile)
 ftp.quit()
 print("FTP closed successfully")
