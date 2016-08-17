56c3101a3043a30685f8a9e6138c1526e9640e97
Sujal <070bex442@ioe.edu.np>
2016-08-17 22:18:21 +0545

    unclear.py ma modulate gareko

--- client/modules/Unclear.py
+++ client/modules/Unclear.py
@@ -9,7 +9,8 @@ fileDir = os.path.dirname(os.path.realpath('__file__'))
 WORDS = []
 PRIORITY = -(maxint + 1)
 
-def logdata(filename,issue_time):
+def logdata(filename,text):
+    issue_time = str(date_string.year) +'-' + str(date_string.month) +'-' + str(date_string.day) +','+ str(date_string.hour) +':'+ str(date_string.minute) +':'+ str(date_string.second)
     filename = os.path.join(fileDir, '../Logs/'+filename)
     filename = os.path.abspath(os.path.realpath(filename))
     with open(filename, "a") as myfile:
@@ -25,13 +26,12 @@ def logdata(filename,issue_time):
 def handle(text, mic, profile):
     date_string = datetime.datetime.now()
     filename = "Missed_Commands.txt"
-    issue_time = str(date_string.year) +'-' + str(date_string.month) +'-' + str(date_string.day) +','+ str(date_string.hour) +':'+ str(date_string.minute) +':'+ str(date_string.second)
+    
     messages = ["I'm sorry, could you repeat that?",
                 "My apologies, could you try saying that again?",
                 "Say that again?", "I beg your pardon?","Sorry I miss this command I would upload this request and hope next time you'll get execution for this command","Sorry Try any other command"]
-
     message = random.choice(messages)
-    logdata(filename,issue_time)
+    logdata(filename,text)
     mic.say(message)
 
 
