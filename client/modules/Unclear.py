5aaee747a816a0e02f23a289de2d60d18e7668ed
Sujal <070bex442@ioe.edu.np>
2016-08-17 22:32:52 +0545

    unclear.py ma modulate gareko

--- client/modules/Unclear.py
+++ client/modules/Unclear.py
@@ -10,6 +10,7 @@ WORDS = []
 PRIORITY = -(maxint + 1)
 
 def logdata(filename,text):
+    date_string = datetime.datetime.now()
     issue_time = str(date_string.year) +'-' + str(date_string.month) +'-' + str(date_string.day) +' , '+ str(date_string.hour) +':'+ str(date_string.minute) +':'+ str(date_string.second)
     filename = os.path.join(fileDir, '../Logs/'+filename)
     filename = os.path.abspath(os.path.realpath(filename))
@@ -24,9 +25,7 @@ def logdata(filename,text):
 
 
 def handle(text, mic, profile):
-    date_string = datetime.datetime.now()
     filename = "Missed_Commands.txt"
-    
     messages = ["I'm sorry, could you repeat that?",
                 "My apologies, could you try saying that again?",
                 "Say that again?", "I beg your pardon?","Sorry I miss this command I would upload this request and hope next time you'll get execution for this command","Sorry Try any other command"]
