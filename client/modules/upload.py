f03f6a1daa29a4e860427dde34e8f84feccc44a1
Sujal <070bex442@ioe.edu.np>
2016-08-17 22:05:30 +0545

    updated upload.py in modules and front

--- client/modules/upload.py
+++ client/modules/upload.py
@@ -8,17 +8,22 @@ fileDir = os.path.dirname(os.path.realpath('__file__'))
 WORDS = ["update","DATABASE","database","UPLOAD","upload",]
 PRIORITY = 1
 
-def isValid(text):
-	#TO know the input is for Selfie-Bot
-    return bool(re.search(r'\b(UPDATE|upload|Database)\b', text, re.IGNORECASE))
- 
-def handle(text,mic,profile):
-	filename = "Missed_Commands.txt"
+def updatedatabase(filename):
 	ftp = ftplib.FTP('ftp.offerharu.com')
 	ftp.login('jarvisai','minor#468&Net')
 	ftp.cwd('Data')
 	myfile = open('/home/pi/Logs/Missed_Commands.txt','rb')
 	ftp.storlines('STOR ' + filename , myfile)
 	ftp.quit()
+	return
+
+
+def isValid(text):
+	#TO know the input is for Selfie-Bot
+    return bool(re.search(r'\b(UPDATE|upload|Database)\b', text, re.IGNORECASE))
+ 
+def handle(text,mic,profile):
+	filename = "Missed_Commands.txt"
+	updatedatabase(filename)
 	mic.say("Database updated and closed successfully")	
 	return	
