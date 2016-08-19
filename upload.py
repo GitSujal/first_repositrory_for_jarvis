1790f4a549c211841c988074404135257c2eca0e
Sujal <070bex442@ioe.edu.np>
2016-08-19 19:07:18 +0545

    this is test

--- upload.py
+++ upload.py
@@ -2,7 +2,7 @@
 import ftplib
 filename = "Missed_Comm.txt"
 ftp = ftplib.FTP('ftp.offerharu.com')
-ftp.login('jarvisai','minor#468&Net')
+ftp.login('jarvisai','password')
 ftp.cwd('Data')
 myfile = open('/Users/SuZal/Desktop/first_repositrory_for_jarvis/Logs/Missed_Comm.txt','rb')
 ftp.storlines('STOR ' + filename , myfile)
